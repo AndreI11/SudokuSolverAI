1e64cfdf1e557b52cbccfda078936fb0396a0acf
riyt <edyounis123@gmail.com>
2016-02-06 19:13:17 -0800

    Added Namespace and furthur improved structure
    Started to populate sudokuFile.cpp,. Also changed up sudokuFile.h

--- src/SudokuFile.h
+++ src/SudokuFile.h
@@ -1,35 +1,39 @@
 #ifndef SUDOKUFILE_H
 #define SUDOKUFILE_H
 
-class SudokuFile
+namespace sudoku_namespace
 {
+	class SudokuFile
+	{
 
-public:
-	// Constructors
-	SudokuFile( void );
-	SudokuFile( int N, int p, int q, int ** board );
+	public:
+		// Constructors
+		SudokuFile( void );
+		SudokuFile( int N, int p, int q, int ** board );
 
-	// Information
-	int getP( void ) const;
-	int getQ( void ) const;
-	int getN( void ) const;
-	int** getBoard( void ) const;
+		// Information
+		int getP( void ) const;
+		int getQ( void ) const;
+		int getN( void ) const;
+		int** getBoard( void ) const;
 
-	// Changers
-	void setP( int p );
-	void setQ( int q );
-	void setN( int n );
-	void setBoard( int** board );
+		// Changers
+		void setP( int p );
+		void setQ( int q );
+		void setN( int n );
+		void setBoard( int** board );
+			
+		// operators
+		friend std::istream &operator>>( std::istream &source, SudokuFile &dest );
+		friend std::ostream &operator<<( std::ostream &dest, const SudokuFile &source );
 
-	// operators
-	std::ostream& operator<< ( std::ostream& os );
+	private:
+		int p;
+		int q;
+		int N;
+		int** board;
 
-private:
-	int p;
-	int q;
-	int N;
-	int** board;
-
-};
+	};
+}
 
 #endif
