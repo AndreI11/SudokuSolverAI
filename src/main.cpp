7110e7cfa74c0a343780b7c29e44a08815595506
riyt <edyounis123@gmail.com>
2016-02-06 18:26:03 -0800

    Check Argument Length
    Added a check for at least 3 command line arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "Tools.h"
+using namespace std;
+using namespace tools;
 
-int main(int argc, char* argv[])
+int main( int argc, char* argv[] )
 {
+	if ( argc < 3 )
+	{
+		cout << "Invalid number of arguments. Must include input file name, output file name, and timeout.";
+		return 0; // quit
+	}
 	// Load Sudoku File
 	// Solve Sudoku Puzzle
 	// Publish Results
